fcdb3414d8bf4cd1b5ae540aa395f5f540c0c356
abdulzai <abdulzai.adib@gmail.com>
2025-10-15 15:37:27 -0700

    Replace Page component with AuroraDashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-"use client";
+import AuroraDashboard from "@/components/AuroraDashboard";
 
 export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1 style={{ fontSize: 28, marginBottom: 12 }}>Aurora EA</h1>
-      <p>UI is up. Go to Settings to connect Gmail/Outlook.</p>
-    </main>
-  );
+  return <AuroraDashboard />;
 }
