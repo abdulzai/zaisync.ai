0486510ce805eae98a7e00dfd43dec0b60d9f9c3
abdulzai <abdulzai.adib@gmail.com>
2025-10-18 16:45:13 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,9 @@
-content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"]
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: { extend: {} },
+  plugins: []
+};
