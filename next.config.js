5e2b32ea844e7c2666424043fc0311cddee2b61d
abdulzai <abdulzai.adib@gmail.com>
2025-10-17 22:18:00 -0700

    Enable experimental app directory support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { appDir: true }, // ensure app/ is used
 };
 
 module.exports = nextConfig;
