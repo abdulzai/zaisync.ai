100523e15a765f587cb27cefba499afa0fd1ad92
abdulzai <abdulzai.adib@gmail.com>
2025-11-12 15:53:25 -0800

    Update route.ts

--- app/api/outlook/meetings/route.ts
+++ app/api/outlook/meetings/route.ts
@@ -1,7 +1,7 @@
 // app/api/outlook/meetings/route.ts
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "../../../../lib/authOptions";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
