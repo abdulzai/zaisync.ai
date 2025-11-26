5b67bd4c2bbf20fcda1191431851f7dc4264b187
abdulzai <abdulzai.adib@gmail.com>
2025-11-25 20:47:09 -0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 // app/api/auth/[...nextauth]/route.ts
+
 import NextAuth from "next-auth";
-import { authOptions } from "../../../../lib/authOptions";
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 
